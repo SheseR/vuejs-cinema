05f0d3ffba598439ed68a3a61aca26ea8801bbd2
Misha <basenko.mykhaylo@gmail.com>
2017-07-17 21:02:42 +0300

    make sceleton

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from 'vue';
+import './style.scss';
+import genres from './util/genres';
+
+
+new Vue({
+    el: "#app",
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                            <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                        </div>`,
+            data() {
+                return {
+                    movies: [
+                        { title: 'pup fiction' },
+                        { title: 'One home' },
+                        { title: 'Terminator' },
+                    ]
+                }
+
+            }
+        },
+        'movie-filter':{
+            data() {
+                return {
+                    genres
+                }
+            },
+            template: `<div id="movie-filter">
+                            <h2>Result of filter</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                            </div>                           
+                        </div>`,
+            components: {
+                'check-filter' : {
+                    template: `<div class="check-filter">
+                                    <span class="checkbox"></span>
+                                    <span class="check-filter-title">{{ title }}</span>
+                                </div>`,
+                    props: [ 'title' ]
+                }
+            }
+        }
+    }
+});
